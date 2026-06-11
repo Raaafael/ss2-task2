54338c032a28fc6782f6c2f5b6192981771fb5af
RafaelJPEG <188954702+RafaelJPEG@users.noreply.github.com>
2026-06-11 09:56:21 +0800

    push react package

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// API KEY = 'https://jsonplaceholder.typicode.com/todos/1'
+
+import axios from "axios"
+import { useState, useEffect } from "react"
+
+export default function App() {
+
+
+  return (
+    <>hello world!</>
+  )
+};
