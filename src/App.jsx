e7bdf6524d62dd31b18f7def732948b161812248
RafaelJPEG <188954702+RafaelJPEG@users.noreply.github.com>
2026-06-11 16:09:29 +0800

    add fake api and content

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-// API KEY = 'https://jsonplaceholder.typicode.com/todos/1'
+// API KEY = https://fakestoreapi.com/products
 
-import axios from "axios"
+import axios from "axios";
 import { useState, useEffect } from "react"
 
-const API_KEY = "https://jsonplaceholder.typicode.com/todos/1"
+const API_KEY = "https://fakestoreapi.com/products"
 
 export default function App() {
-  const [user, setUsers] = useState()
+  const [users, setUsers] = useState([])
 
   const handleFetchData = async () => {
     try {
+      const result = await axios.get(API_KEY);
+      setUsers(result.data)
 
     } catch (e) {
 
@@ -17,13 +19,16 @@ export default function App() {
   };
 
   useEffect(() => {
+    handleFetchData();
     
   }, []);
 
   return (
     <>
       {
-
+        users?.map(user => (
+          <p>{user.id}. {user.title}</p>
+        ))
       }
     </>
   )
